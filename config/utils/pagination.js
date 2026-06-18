69331c1298de2374d66a08ab74bda65a7b0391da
Ojugbo Progress <lingardj852@gmail.com>
2026-06-18 06:05:30 -0700

    Delete config/utils/pagination.js

--- config/utils/pagination.js
+++
@@ -1,8 +0,0 @@
-const getPagination = (query) => {
-  const page = Math.max(1, parseInt(query.page) || 1);
-  const limit = Math.min(100, Math.max(1, parseInt(query.limit) || 10));
-  const skip = (page - 1) * limit;
-  return { page, limit, skip };
-};
-
-module.exports = { getPagination };
